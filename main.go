55e1be4dc9a208ace352c3465d3cc9130daa4765
Ronny Bangsund <github@neuralshortcircuit.com>
2019-08-25 22:18:59 +0200

    Fixed path specification.

--- main.go
+++ main.go
@@ -8,16 +8,14 @@ import (
 
 var opts struct {
 	opt.DefaultHelp
-	Version  bool `short:"V" long:"version" description:"Prints version and exits."`
-	Verbose  bool `short:"v" description:"Verbose output. Shows progress of checksumming for each file, not just the list of duplicates."`
-	List     bool `short:"l" long:"list" description:"List duplicates only."`
-	Symlink  bool `long:"symlink" description:"Symlink all duplicates to the first file."`
-	Hardlink bool `long:"hardlink" description:"Hardlink all duplicates to the first file."`
-	Remove   bool `long:"remove" description:"Remove duplicates of the first file."`
-	Tens     bool `short:"s" long:"si" description:"Use SI numbers." default-value:"true"`
-	Args     struct {
-		Path string `positional-arg-name:"DIRECTORY" decription:"Directory to look deeply into for duplicates." default-value:"."`
-	} `positional-args:"true"`
+	Version  bool   `short:"V" long:"version" description:"Prints version and exits."`
+	Verbose  bool   `short:"v" description:"Verbose output. Shows progress of checksumming for each file, not just the list of duplicates."`
+	List     bool   `short:"l" long:"list" description:"List duplicates only."`
+	Symlink  bool   `long:"symlink" description:"Symlink all duplicates to the first file."`
+	Hardlink bool   `long:"hardlink" description:"Hardlink all duplicates to the first file."`
+	Remove   bool   `long:"remove" description:"Remove duplicates of the first file."`
+	Tens     bool   `short:"s" long:"si" description:"Use SI numbers." default-value:"true"`
+	Path     string `placeholder:"DIRECTORY" help:"Directory to look deeply into for duplicates." default-value:"."`
 }
 
 func main() {
@@ -32,7 +30,7 @@ func main() {
 		return
 	}
 
-	dir, err := filepath.Abs(opts.Args.Path)
+	dir, err := filepath.Abs(opts.Path)
 	if err != nil {
 		pr("Error: %s", err.Error())
 		return
